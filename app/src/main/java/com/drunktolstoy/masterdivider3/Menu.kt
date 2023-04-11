3c82f3cd4b369a4778466c20a5aab2d8fe5726b3
or israeli <or.p.israeli@gmail.com>
2023-04-11 22:51:46 +0300

    fixing auth

--- app/src/main/java/com/drunktolstoy/masterdivider3/Menu.kt
+++ app/src/main/java/com/drunktolstoy/masterdivider3/Menu.kt
@@ -2,17 +2,22 @@ package com.drunktolstoy.masterdivider3
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.drunktolstoy.masterdivider3.databinding.ActivityMenuBinding
+import com.google.android.gms.games.AuthenticationResult
 import com.google.android.gms.games.PlayGames
 import com.google.android.gms.games.PlayGamesSdk
+import com.google.android.gms.games.Player
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
 
 class Menu : AppCompatActivity() {
     private lateinit var binding: ActivityMenuBinding
     private lateinit var auth: FirebaseAuth
-
+    private var user: Player? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuBinding.inflate(layoutInflater)
@@ -21,22 +26,37 @@ class Menu : AppCompatActivity() {
 
         PlayGamesSdk.initialize(this);
         val gamesSignInClient = PlayGames.getGamesSignInClient(this)
-        gamesSignInClient.signIn()
-            .addOnCompleteListener { _ ->
-                gamesSignInClient.isAuthenticated.addOnCompleteListener { isAuthedTask1 ->
-                    PlayGames.getPlayersClient(this).currentPlayer.addOnCompleteListener { mTask ->
-                        print(
-                            mTask.result.displayName
-                        )
-                    }
-                }
-            }
+        gamesSignInClient.signIn().addOnCompleteListener(onSigIn)
 
 
         binding.button.setOnClickListener { _ ->
-            intent = Intent(this, MainActivity::class.java)
-
-            startActivity(intent)
+            if (user != null) {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.button.setOnClickListener { _ ->
+                    PlayGames.getGamesSignInClient(this).signIn().addOnCompleteListener(onSigIn)
+                }
+            }
         };
     }
+    private val onSigIn =
+        OnCompleteListener<AuthenticationResult> { _->
+        val gamesSignInClient = PlayGames.getGamesSignInClient(this)
+        gamesSignInClient.isAuthenticated.addOnCompleteListener { isAuthed ->
+            if (isAuthed.isSuccessful && isAuthed.result.isAuthenticated) {
+                PlayGames.getPlayersClient(this).currentPlayer.addOnCompleteListener { mTask ->
+                    user = mTask.result
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Opps, auth failed, please try to sign in manually",
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.button.text = getString(R.string.SignInButon)
+            }
+        }
+    }
+
 }
